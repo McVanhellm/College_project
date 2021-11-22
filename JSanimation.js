892f28f022357b2b1312edc880781ca337ed2be1
McVanhellm <mcavnhellm@gmail.com>
2021-11-22 17:44:57 +0300

    inition commit

---
+++ JSanimation.js
@@ -0,0 +1,24 @@
+VANTA.NET({
+ el: "#intro",
+ mouseControls: true,
+ touchControls: true,
+ gyroControls: false,
+ minHeight: 800.00,
+ minWidth: 220.00,
+ scale: 1.00,
+ scaleMobile: 1.00,
+ color: 0x756969,
+ backgroundColor: 0x17141e,
+ points: 11.00,
+ maxDistance: 22.00,
+ spacing: 16.00
+})
+window.addEventListener("scroll", function(){
+ let scrollPos = this.window.scrollY;
+if(scrollPos > 199){
+header.classList.add('red');
+} else{
+header.classList.remove('red');  
+}
+console.log(scrollPos);
+});
