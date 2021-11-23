8797eb50d30ad76e6df8b2ef0c9dedeb67f2eace
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2021-11-24 02:45:41 +0300

    Refactoring code

--- js/JSanimation.js
+++ js/JSanimation.js
@@ -1,25 +1,31 @@
-VANTA.NET({//Анимация фона частиц//
- el: "#intro",
- mouseControls: true,
- touchControls: true,
- gyroControls: false,
- minHeight: 800.00,
- minWidth: 220.00,
- scale: 1.00,
- scaleMobile: 1.00,
- color: 0x756969,
- backgroundColor: 0x17141e,
- points: 11.00,
- maxDistance: 22.00,
- spacing: 16.00
+// Анимация фона частиц
+VANTA.NET({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 800.00,
+    minWidth: 220.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x756969,
+    backgroundColor: 0x17141e,
+    points: 11.00,
+    maxDistance: 22.00,
+    spacing: 16.00
 })
-//Тут писал Федя
-window.addEventListener("scroll", function(){
- let scrollPos = this.window.scrollY;
-if(scrollPos > 199){
-header.classList.add('red');
-} else{
-header.classList.remove('red');  
-}
-console.log(scrollPos);
+
+//Код от феди
+window.addEventListener("scroll", function()
+{
+    let scrollPos = this.window.scrollY;
+    if(scrollPos > 199)
+    {
+        header.classList.add('red');
+    } 
+    else
+    {
+        header.classList.remove('red');  
+    }
+    console.log(scrollPos);
 });
