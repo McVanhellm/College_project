7fb8a1603f1f3edac6c843f2902091d3d70c860a
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2021-11-23 21:40:14 +0300

    add Controllers

---
+++ application/Views/account/login.php
@@ -0,0 +1,10 @@
+<?php 
+	
+	require_once("application/Core/Controller.php");
+
+	class LoginController extends Controller 
+	{
+		
+	}
+
+?>
---
+++ application/Views/account/register.php
@@ -0,0 +1,10 @@
+<?php 
+	
+	require_once("application/Core/Controller.php");
+
+	class RegisterController extends Controller 
+	{
+		
+	}
+
+?>
