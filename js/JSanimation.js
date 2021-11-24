6d8b8127f3e6c4a9df124287fd8a51ec9143f4e9
Alistair <67331367+McVanhellm@users.noreply.github.com>
2021-11-24 11:37:31 +0300

    Innition commit

--- js/JSanimation.js
+++ js/JSanimation.js
@@ -8,24 +8,11 @@ VANTA.NET({
     minWidth: 220.00,
     scale: 1.00,
     scaleMobile: 1.00,
-    color: 0x756969,
-    backgroundColor: 0x17141e,
+    color: 0x699BE5,
+    backgroundColor: 0xFFFFFF,
     points: 11.00,
     maxDistance: 22.00,
     spacing: 16.00
 })
 
 //Код от феди
-window.addEventListener("scroll", function()
-{
-    let scrollPos = this.window.scrollY;
-    if(scrollPos > 199)
-    {
-        header.classList.add('red');
-    } 
-    else
-    {
-        header.classList.remove('red');  
-    }
-    console.log(scrollPos);
-});
