1bb9bb2e7744cb57c925cda432e936b5cb7c6306
McVanhellm <mcvanhellm@gmail.com>
2021-11-27 16:02:35 +0300

    innition commit

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
-
 	/*
-
 		CMS by xoheveras(Egor Udovin)
 		https://github.com/xoheveras/CMS
-
 	*/
-
 	# Connecting the main classes
 	require_once("application/app.php");
-
 	# Routing
 	$router = new Router();
 
