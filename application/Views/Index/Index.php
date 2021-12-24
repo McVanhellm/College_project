11e47fe2e967fabee2d9e57d53788e1a098a0ffc
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2021-12-24 19:21:54 +0300

    Update Index.php

--- application/Views/Index/Index.php
+++ application/Views/Index/Index.php
@@ -1,25 +1,24 @@
 <html>
- <link rel="stylesheet" type="text/css" href="vendor/css/Style.css">
- <head></head>
- <body>
-  <div class="intro" id="intro">
-   <div class="header-box">
-    <div class="logo-area">
-     <div class="logo-box"><img src="logo.png" alt=""></div>
-   </div>
-   <img class="back-img" src="vendor/image/bgindex.png" alt="">
-   <div align="right" class="button-box">
-    <a href="account/login">Войти</a>
-    <a href="account/register">Регистрация</a>
-   </div>
-   </div>
-   <div class="info-box">
-    <div class="title">Лучший способ выучить физику</div>
-    <div class="sub-text">Множество интересных задач, интерактивный режим обучения.<br> Участвуй в общем рейтинге и стань лучшим.</div>
-    <div align="center" class="btn-start">Начать</div>
-   </div>
-  </div>
- </body>
+    <link rel="stylesheet" type="text/css" href="vendor/css/Style.css">
+    <body>
+        <div class="intro" id="intro">
+            <div class="header-box">
+                <div class="logo-area">
+                    <div class="logo-box"><img src="logo.png" alt=""></div>
+                </div>
+                <img class="back-img" src="vendor/image/bgindex.png" alt="">
+                <div align="right" class="button-box">
+                    <a href="account/login">Войти</a>
+                    <a href="account/register">Регистрация</a>
+                </div>
+            </div>
+            <div class="info-box">
+                <div class="title">Лучший способ выучить физику</div>
+                <div class="sub-text">Множество интересных задач, интерактивный режим обучения.<br> Участвуй в общем рейтинге и стань лучшим.</div>
+                <div align="center" class="btn-start">Начать</div>
+            </div>
+        </div>
+    </body>
 </html>
 <script src="vendor/js/JSsector.js"></script>
 <script src="vendor/js/three.min.js"></script>
